3ffdf5e5d728fcf7aa2d0ecdcd4bf353c73afb54
Sebastian Amaya Perez <sebastian.amaya1@udea.edu.co>
2026-06-02 00:41:30 -0500

    Config

--- backend/src/main/java/com/ebp04/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/ebp04/backend/config/SecurityConfig.java
@@ -70,7 +70,7 @@ public class SecurityConfig {
                 "http://localhost:5173",
                 "http://127.0.0.1:5173",
                 "https://ebp-04-frontend.vercel.app",
-                "https://ebp-04.vercel.app/"
+                "https://ebp-04.vercel.app"
         ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         // configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
